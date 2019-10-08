7d90ad2635ef753892597189ce56a76f3a3ba9a6
Foureau <e.foureau@students.ephec.be>
2019-10-08 12:21:39 +0200

    Sepération fonction

--- F1.c
+++ F1.c
@@ -2,19 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-double randGen(){
-	srand(time(NULL));
-	double tempsTour[4], s;
-	int i, n = 3;
-	for ( i = 0; i < n; i++ ){
-		s = (( rand() % 1600 ) / 100 ) + 35;
-		tempsTour[i] = s;
-		tempsTour[3] += s;
-		printf("S%d : %.2f secondes\n", (i+1), tempsTour[i]);
-	}
-	printf("Temps du tour : %.2f secondes\n", tempsTour[3]);
-	return 0;
+struct voiture {
+    int num;
+    double s1, s2, s3, tempsTotal;
 }
+
+
 int main (int argc, char *argv[]) {
 
     int pid_fils;
@@ -27,7 +20,6 @@ int main (int argc, char *argv[]) {
     
         pid_fils = fork();
         if (pid_fils == 0) {
-            int num = voitures[i];
             printf("La voiture avec le numéro %d a fait la section 1 avec le temps %d \n",num, timer());
             //printf("Le fils avec pid %d et num %d et pere a pid %d\n",getpid(),num, getppid()); 
             exit(0);
